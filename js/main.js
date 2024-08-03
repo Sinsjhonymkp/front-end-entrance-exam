1065a519f3d78e4a93187bbd7875ab6ee7af60c0
Sinsjhonymkp <tohaantoha96@gmail.com>
2024-08-03 12:43:43 +0300

    init: create layout

--- js/main.js
+++ js/main.js
@@ -1,24 +1,71 @@
-import '../css/style.css'
-import javascriptLogo from '../javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "../css/style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const exprienseCard = document.querySelectorAll(".expiriense__wrapper-item");
 
-setupCounter(document.querySelector('#counter'))
+exprienseCard.forEach((item) => {
+  const newBtn = document.createElement("button");
+  newBtn.textContent = "most resent";
+  newBtn.classList.add("experiense__btn");
+  item.appendChild(newBtn);
+  newBtn.addEventListener("click", (btn) => {
+    item.classList.toggle("expiriense__wrapper-item-active");
+    item.classList.toggle("expiriense__wrapper-item");
+    newBtn.classList.toggle("experiense__btn-active");
+  });
+  item.addEventListener("click", () => {
+    item.setAttribute("contenteditable", "true");
+    item.style.cursor = "text";
+  });
+  item.addEventListener("mouseleave", () => {
+    item.setAttribute("contenteditable", "false");
+    item.style.cursor = "default";
+  });
+});
+const mainSections = document.querySelectorAll(".main > div");
+mainSections.forEach((section) => {
+  section.setAttribute("draggable", true);
+
+  section.addEventListener("dragstart", handleDragStart);
+  section.addEventListener("dragover", handleDragOver);
+  section.addEventListener("drop", handleDrop);
+  section.addEventListener("dragenter", handleDragEnter);
+  section.addEventListener("dragleave", handleDragLeave);
+});
+
+let draggedSection = null;
+
+function handleDragStart(event) {
+  draggedSection = this;
+  event.dataTransfer.effectAllowed = "move";
+  event.dataTransfer.setData("text/html", this.innerHTML);
+  this.classList.add("dragging");
+}
+
+function handleDragOver(event) {
+  event.preventDefault();
+  return false;
+}
+
+function handleDrop(event) {
+  event.stopPropagation();
+  if (draggedSection !== this) {
+    draggedSection.innerHTML = this.innerHTML;
+    this.innerHTML = event.dataTransfer.getData("text/html");
+  }
+  return false;
+}
+
+function handleDragEnter(event) {
+  this.classList.add("over");
+}
+
+function handleDragLeave(event) {
+  this.classList.remove("over");
+}
+
+mainSections.forEach((section) => {
+  section.addEventListener("dragend", function () {
+    this.classList.remove("dragging");
+    mainSections.forEach((sec) => sec.classList.remove("over"));
+  });
+});
